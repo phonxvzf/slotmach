3175a8a5ea70776cfe887434e950c8113312a268
elm-kok <polindays@gmail.com>
2017-11-23 18:37:45 +0700

    before pull

--- src/core/settings/Settings.java
+++ src/core/settings/Settings.java
@@ -6,7 +6,7 @@ public class Settings {
 	public static final double SLOT_DEFAULT_VELOCITY = 2000.0f;
 	public static final double SLOT_DEFAULT_COLUMN_HEIGHT = 1000.0f;
 	public static final double SLOT_DEFAULT_WIDTH = 50.0f;
-	public static final double SLOT_SLOWDOWN_ACCEL = -800.0f;
+	public static final double SLOT_SLOWDOWN_ACCEL = -1000.0f;
 	public static final double SLOT_MIN_VELOCITY = 400.0f;
 	public static final int SLOT_DEFAULT_COLUMNS = 3;
 }
