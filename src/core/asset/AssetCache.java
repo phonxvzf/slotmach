76056eea2b07b8b65220bdd4cbb298589fa3f23b
Phon Chitnelawong <phon.chitnelawong@gmail.com>
2017-11-24 17:46:50 +0700

    Simplify asset loading

--- src/core/asset/AssetCache.java
+++ src/core/asset/AssetCache.java
@@ -10,11 +10,11 @@ public final class AssetCache {
 	private static final Map<AssetID, Image> imageCache = new HashMap<AssetID, Image>();
 	
 	public static void loadAssets() {
-		imageCache.put(AssetID.TEST_IMG, new Image("file:assets/test.png"));
-		imageCache.put(AssetID.K_IMG, new Image("file:assets/K.png"));
-		imageCache.put(AssetID.O_IMG, new Image("file:assets/O.png"));
-		imageCache.put(AssetID.FROZEN_IMG, new Image("file:assets/frozen.png"));
-		imageCache.put(AssetID.MPBAR_IMG, new Image("file:assets/mpbar.png"));
+		imageCache.put(AssetID.TEST_IMG, new Image("img/test.png"));
+		imageCache.put(AssetID.K_IMG, new Image("img/K.png"));
+		imageCache.put(AssetID.O_IMG, new Image("img/O.png"));
+		imageCache.put(AssetID.FROZEN_IMG, new Image("img/frozen.png"));
+		imageCache.put(AssetID.MPBAR_IMG, new Image("img/mpbar.png"));
 	}
 	
 	public static Image getImage(AssetID id) {
