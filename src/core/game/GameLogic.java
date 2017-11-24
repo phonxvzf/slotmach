adff2f2a668c2c9729a44dc548b047ea7fa62dcc
elm-kok <polindays@gmail.com>
2017-11-24 10:24:47 +0700

    mpBar

--- src/core/game/GameLogic.java
+++ src/core/game/GameLogic.java
@@ -5,26 +5,26 @@ import javafx.application.Platform;
 import javafx.scene.input.KeyCode;
 
 public class GameLogic {
-	
+
 	private Thread logicThread;
 	private GameModel gameModel;
 	private boolean isRunning;
-	
+
 	public GameLogic(GameModel model) {
 		gameModel = model;
 		isRunning = false;
 	}
-	
+
 	public void startGame() {
 		isRunning = true;
 		logicThread = new Thread(this::logicLoop, "Logic Thread");
 		logicThread.start();
 	}
-	
+
 	public void stopGame() {
 		isRunning = false;
 	}
-	
+
 	private void logicLoop() {
 		long lastLoopStartTime = System.nanoTime();
 		while (isRunning) {
@@ -32,8 +32,7 @@ public class GameLogic {
 			if (dt >= Settings.UPDATE_LOOP_TIME) {
 				updateGame(dt); // FIXED FPS
 				lastLoopStartTime += dt;
-			}
-			else {
+			} else {
 				try {
 					Thread.sleep((Settings.UPDATE_LOOP_TIME - dt) / 1000000);
 				} catch (InterruptedException e) {
@@ -43,26 +42,27 @@ public class GameLogic {
 
 		}
 	}
-	
+
 	private void updateGame(long dt) {
 		// Be aware of deadlock
 		// Check for events
 		if (InputHandler.isKeyDown(KeyCode.S)) {
 			gameModel.slotMachine.slowDown();
-			gameModel.gameState.giveMana(-5);
-		}
-		else {
+			gameModel.gameState.giveMana(-Settings.SKILL_FREEZE_MPRATE * dt / 1e9);
+		} else {
+			if (gameModel.gameState.getMana() <= Settings.PLAYER_MAX_MANA) {
+				gameModel.gameState.giveMana(Settings.SKILL_FREEZE_MPRATE * dt / 1e9);
+			}
 			gameModel.slotMachine.returnSpeed();
 		}
-		
+
 		KeyCode triggeredKey;
 		while ((triggeredKey = InputHandler.pollTriggeredKey()) != null) {
 			if (triggeredKey == KeyCode.SPACE) {
 				if (!gameModel.slotMachine.pull()) {
 					gameModel.slotMachine.reset();
 				}
-			}
-			else if (triggeredKey == KeyCode.ESCAPE) {
+			} else if (triggeredKey == KeyCode.ESCAPE) {
 				Platform.exit();
 				System.exit(0);
 			}
