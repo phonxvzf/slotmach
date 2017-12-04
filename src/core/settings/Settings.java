6fd97620d3618abf49c51909c485810ebad59c46
Phon Chitnelawong <phon.chitnelawong@gmail.com>
2017-12-04 22:46:56 +0700

    Format code and remove sysout

--- src/core/settings/Settings.java
+++ src/core/settings/Settings.java
@@ -12,9 +12,9 @@ public class Settings {
 	// Game logic options
 	public static final int UPDATE_RATE = 25;
 	public static final long UPDATE_LOOP_TIME = 1000000000 / UPDATE_RATE;
-
+	
 	// General in-game options
-	public static final double SLOT_DEFAULT_VELOCITY = 500.0f;
+	public static final double SLOT_DEFAULT_VELOCITY = 2000.0f;
 	public static final double SLOT_DEFAULT_COLUMN_HEIGHT = GAME_CANVAS_HEIGHT;
 	public static final double SLOT_DEFAULT_WIDTH = 50.0f;
 	public static final double SLOT_SLOWDOWN_ACCEL = -800.0f;
